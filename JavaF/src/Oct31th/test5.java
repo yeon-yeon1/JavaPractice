5502a288f611e075ac3fb89e3ed275dfa0cf4298
yeon-yeon1 <nojk0106@naver.com>
2025-10-31 16:42:25 +0900

    🐛 Fix: 예제 5 객체 생성 for문으로
    Fix: 예제 5 객체 생성 for문으로

--- JavaF/src/Oct31th/test5.java
+++ JavaF/src/Oct31th/test5.java
@@ -1,3 +1,30 @@
+//package Oct31th;
+//
+//public class test5 {
+//    String aa; int bb;
+//    void cc(){
+//        System.out.printf("객체 Array \n");
+//    }
+//
+//    public static void main(String[] args) {
+//        test5[] arr =new test5[3];
+//        arr[0] = new test5();
+//        arr[1] = new test5();
+//        arr[2] = new test5();
+//
+//        for(int i=0;i<3;i++) {
+//            arr[i].aa="AAA";
+//            arr[i].bb=i;
+//        }
+//
+//        for(int i=0;i<3;i++) {
+//            System.out.printf("Array arr[%d].aa=%s,arr[%d].bb=%d\n",i,arr[i].aa,i,arr[i].bb);
+//        }
+//    }
+//}
+
+// 객체 생성 포문으로 돌려서 넣기
+
 package Oct31th;
 
 public class test5 {
@@ -7,20 +34,21 @@ public class test5 {
     }
 
     public static void main(String[] args) {
-        test5[] arr =new test5[3];
-        arr[0] = new test5();
-        arr[1] = new test5();
-        arr[2] = new test5();
+        test5[] arr = new test5[3];
+
+        for(int i=0; i < arr.length; i++){
+            arr[i] = new test5();
+        }
 
         for(int i=0;i<3;i++) {
             arr[i].aa="AAA";
             arr[i].bb=i;
         }
-
         for(int i=0;i<3;i++) {
             System.out.printf("Array arr[%d].aa=%s,arr[%d].bb=%d\n",i,arr[i].aa,i,arr[i].bb);
         }
     }
 }
+
 
 
